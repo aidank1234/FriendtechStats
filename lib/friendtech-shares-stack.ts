3bfd6a52f315f6e5df7271e9ad0aad91964a3a22
Aidan Kaiser <apkaiser@wisc.edu>
2024-10-14 11:34:51 -0700

    Working, past two weeks

--- lib/friendtech-shares-stack.ts
+++ lib/friendtech-shares-stack.ts
@@ -1,16 +1,39 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
 
 export class FriendtechSharesStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        // Retrieve environment variables from context
+        const providerUrl = this.node.tryGetContext('PROVIDER_URL');
+        const contractAddress = this.node.tryGetContext('CONTRACT_ADDRESS');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FriendtechSharesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // Define the Lambda function
+        const friendtechLambda = new lambda.Function(this, 'FriendtechLambdaHandler', {
+          runtime: lambda.Runtime.NODEJS_18_X,
+          code: lambda.Code.fromAsset(path.join(__dirname, '../lambda'), {
+            // Exclude unnecessary files
+            exclude: ['*.ts', 'tsconfig.json'],
+          }),
+          handler: 'index.handler',
+          timeout: cdk.Duration.seconds(500),
+          environment: {
+            PROVIDER_URL: providerUrl,
+            CONTRACT_ADDRESS: contractAddress,
+          },
+        });
+
+        // Define the API Gateway
+        const api = new apigateway.RestApi(this, 'friendtech-api', {
+            restApiName: 'Friendtech Service',
+            description: 'This service retrieves creator fees from the FriendTech contract.',
+        });
+
+        const getIntegration = new apigateway.LambdaIntegration(friendtechLambda);
+        api.root.addMethod('GET', getIntegration); // GET /
+    }
 }
